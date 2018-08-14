df47639d9b4315d1fd04d4a5240b0dd2c43fb707
bioinfoacademy <vijay@graphinformatics.com>
2018-08-14 16:21:49 -0400

    code9 lwpsimple perl
    Sets up the code9 environment for lwpsimple perl modules

---
+++ code9perl.sh
@@ -0,0 +1,6 @@
+sudo yum -y install perl-libwww-perl
+sudo yum -y install perl-CPAN
+sudo yum -y install perl-Net-SSLeay
+sudo yum -y install perl-IO-Socket-SSL
+sudo yum -y install perl-Crypt-SSLeay   
+sudo yum -y install perl-LWP-Protocol-https
