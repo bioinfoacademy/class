acf6d7ab8a182016393535cecd25cce74e236439
Vijay <vijay@graphinformatics.com>
2019-08-11 16:01:54 +0000

    august 2019 test

--- hello.pl
+++ hello.pl
@@ -2,26 +2,8 @@
 
 #Author - Vijay Nagarajan PhD
 #print a string - hello vijay
-print "Hello Shalmika\n";
+print "Hello Raj\n";
 
-#variable
-$DNA = 'ACGGGAGGACGGGAAAATTACTACGGCATTAGC';
-print $DNA,"\n";
-
-#string concatenation
-$DNA2 = 'ATAGTGCCGTGAGAGTGATGTAGTA';
-print "Here are the original two DNA fragments:\n\n";
-print $DNA,"\n";
-print $DNA2,"\n\n";
-$DNA3 = "$DNA$DNA2";
-print "Here is the concatenation of the first two fragments (version 1):\n\n";
-print $DNA3,"\n";
-
-#Transcribe DNA to RNA
-$RNA = $DNA3;
-$RNA =~ s/T/U/g;
-print "Here is the result of transcribing the DNA to RNA:\n\n";
-print "$RNA\n";
 
 
 
