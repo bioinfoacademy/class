5ed9170dac2b67550f1484918921650ac44de905
Vijay <vijay@graphinformatics.com>
2019-08-16 15:48:55 +0000

    my real perl program

---
+++ bighello.pl
@@ -0,0 +1,27 @@
+#!/usr/bin/perl
+
+#Author - Vijay Nagarajan PhD
+#print a string
+print "Hello World\n";
+
+#variable
+$DNA = 'ACGGGAGGACGGGAAAATTACTACGGCATTAGC';
+print $DNA,"\n";
+
+#string concatenation
+$DNA2 = 'ATAGTGCCGTGAGAGTGATGTAGTA';
+print "Here are the original two DNA fragments:\n\n";
+print $DNA,"\n";
+print $DNA2,"\n\n";
+$DNA3 = "$DNA$DNA2";
+print "Here is the concatenation of the first two fragments (version 1):\n\n";
+print $DNA3,"\n";
+
+#Transcribe DNA to RNA
+$RNA = $DNA3;
+$RNA =~ s/T/U/g;
+print "Here is the result of transcribing the DNA to RNA:\n\n";
+print "$RNA\n";
+
+
+
