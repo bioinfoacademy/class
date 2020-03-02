e476db59837af0e17f65f73f6414f0f2023ab310
Vijay <vijay@graphinformatics.com>
2020-03-02 21:47:32 +0000

    my first complete script

--- hello.sh
+++ hello.sh
@@ -1,4 +1,57 @@
 #!/bin/bash
 
-echo "Hello world"
+#echo "Hello world"
+
+HELLO="Hello Vijay"
+
+echo $HELLO
+
+#variable a,b
+a=1
+b=2
+echo $a,$b
+
+#arithmetic operations
+c=$(( a+b ))
+echo "a+b" is $c
+echo "a-b" is $(( a-b ))
+echo "a*b" is $(( a*b ))
+
+myfiles=$( ls ~/ | wc -l )
+echo $myfiles
+
+#user input, get two numbers
+echo "Enter your first number:"
+#read firstnumber
+echo "Enter your second number:"
+#read secondnumber
+
+echo $firstnumber
+echo $secondnumber
+
+#conditionals
+if [ $firstnumber -gt $secondnumber ]
+    then
+        echo $firstnumber "is the larger number"
+fi
+
+#for loop
+aligners='bwa star bowtie tophat trinity'
+for item in $aligners
+    do
+    echo $item
+    done
+
+#function to convert celcius to fahrenheit
+function ctof
+    {
+    ft=$(( $1 * (9/5) + 32 ))
+    echo $ft
+    }
+
+#call the function
+ctof 23
+
+
+
 
