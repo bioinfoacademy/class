8194dda0df62872313cbca6fd2b477aec0ff4180
bioinfoacademy <vijay@graphinformatics.com>
2020-04-17 18:35:35 +0000

    April 17, 2020

--- dnaseq.sh
+++ dnaseq.sh
@@ -1,31 +1,30 @@
 #!/bin/bash
 #Commands to run RNA-Seq analysis
 #Numbers after the hashtag (#) sysmbol corresponds to sections in manual
-#Authors: Vijay Nagarajan PhD, Harold Smith PhD
 
-#####################################
-########  SET-UP ##################
+########  1. SET-UP ##
 
-###Start a new MATE terminal to go to your home folder
-ls 				##display contents
-mkdir mydna   	##replace the FOLDERNAME with a name
-cd mydna 		## go to your folder
+### Start a new MATE terminal to go to your home folder
 
+# 1.1. Display working directory contents
+ls
 
-##copy necessary files
+# Create a new directory named 'mydna'
+mkdir mydna
+
+# Change to the 'mydna' directory
+cd mydna
+
+## 1.2. copy necessary files
 ls ~/Desktop/dnaseq
 ln -s ~/Desktop/dnaseq/human* .
 ln -s ~/Desktop/dnaseq/dad_1.fq.gz .
 ln -s ~/Desktop/dnaseq/dad_2.fq.gz .
 ln -s ~/Desktop/dnaseq/dbSNP* .
 
+############# 2. QC & alignment ###
 
-############################################
-###########################################
-
-#############   PART I. QC & alignment  ######################
-
-#------step 1: QC on fastq file-------
+# 2.1. QC on fastq file
 
 #test files available
 #run fastqc on fastq files (generates .html file: with qc statistics) 
@@ -35,14 +34,19 @@ firefox dad_1_fastqc.html
 
 #view results using x terminal (only works on Xterminal!!)
 
+# 2.2. Index reference genome
 #Already done here due to time!!!!
 #bwa index human.hg38.chr16.fa
 
-#-------step 2: alignment------------
+# 2.3. alignment
 
 #1) using bwa, align fastq files, outputs stderr to log file, compresses the sam file
 #for bwa-mem options: RG=read group: ID-id, LB-library, SM-Sample, PU-platform unit (ex.barcode), PL-platform (sequencer) 
-bwa mem  ##shows options
+
+#show options
+bwa mem
+
+#run alignment
 bwa mem -R "@RG\\tID:dad\\tLB:dad\\tSM:dad\\tPU:FCC189PACXX\\tPL:ILLUMINA" -M human.hg38.chr16.fa dad_1.fq.gz dad_2.fq.gz | gzip > dad.sam.gz
 
 #2)look at the content of the alignment file
@@ -51,65 +55,67 @@ less -S dad.sam.gz
 #3)convert SAM to sorted BAM- time consuming
 picard SortSam INPUT=dad.sam.gz OUTPUT=dad.bam SORT_ORDER=coordinate  
 
-#create index file
+#4)create index file
 samtools index dad.bam 
 
-#view the bam file, notice how it's aligned 
+#5)view the bam file, notice how it's aligned 
 samtools view dad.bam | less -S 
 
-#view the aligned reads 
-samtools tview dad.bam human.hg38.chr16.fa
-#press g, then type "chr16:400100"  
-#q to quit
+#6)view the aligned reads using igv
+source /etc/profile.d/markcbm.sh
+
+#start igv from terminal, by running the command 'igv'
+igv
+#load alignment file dad.bam, load genome from file human.hg38.chr16.fa
+#search for "chr16:400100"  
 
 #--------now you have aligned reads----------------
 #--------------------------------------------------
 
-############   PART II. PRE-PROCESSING ALIGNED READS  ####################
+############ 3. PRE-PROCESSING ALIGNED READS  ##
 
-#1) PICARD Mark Duplicates
+# 3.1. PICARD Mark Duplicates
 picard MarkDuplicates INPUT=dad.bam OUTPUT=dad.chr16.dedup.bam ASSUME_SORTED=true M=dad.chr16.metrics.txt
 
-#2) GATK Base recalibration
+# 3.2. GATK Base recalibration
 source /etc/profile.d/markcbm.sh
+export PATH=/usr/lib/jvm/java-1.8.0-openjdk-amd64/bin/:$PATH
 
 gatk BaseRecalibrator -I dad.bam -R human.hg38.chr16.fa --known-sites dbSNP151.chr16.vcf -O dad.recal_report_chr16.grp
 
-#Recalibrate the aligned bam file
+# 3.3. Recalibrate the aligned bam file
 gatk ApplyBQSR -R human.hg38.chr16.fa -I dad.chr16.dedup.bam -O dad.chr16.recal.bam -bqsr dad.recal_report_chr16.grp
 
+############ 4. VARIANT DISCOVERY  ##
 
-############   PART III. VARIANT DISCOVERY  ####################
-
-# 1) GATK Haplotype caller to derive likelihoods of genotypes, creates gVCF
+# 4.1. GATK Haplotype caller to derive likelihoods of genotypes, creates gVCF
 gatk HaplotypeCaller -R human.hg38.chr16.fa -I dad.chr16.recal.bam -O dad.chr16.g.vcf -ERC GVCF
 
-
-# 2) GATK GenotypeGVCFs 
+# 4.2. GATK GenotypeGVCFs 
 
 #single sample!! 
 gatk GenotypeGVCFs -R human.hg38.chr16.fa -V dad.chr16.g.vcf -O dad.raw.vcf
 
-#extract SNPs and apply hard filter, do the same for indels
-# extracting SNPs
+# 4.3. extract SNPs and apply hard filter, do the same for indels
+# a) extracting SNPs
 gatk SelectVariants -R human.hg38.chr16.fa -V dad.raw.vcf -select-type SNP -O dad_SNPs.vcf 
 
-# applying hard filter
+# b) applying hard filter
 gatk VariantFiltration -R human.hg38.chr16.fa -V dad_SNPs.vcf -O dad.filtered_SNPs.vcf --filter-name "my_filter1" --filter-expression "DP < 3" --filter-name "my_filter2" --filter-expression "QD < 2.0" --filter-name "my_filter3" --filter-expression "MQ < 40.0" --filter-name "my_filter4" --filter-expression "FS > 60.0" --filter-name "my_filter5" --filter-expression "SOR > 3.0" --filter-name "my_filter6" --filter-expression "MQRankSum < -12.5" --filter-name "my_filter7" --filter-expression "ReadPosRankSum < -8.0"
 
-# extracting INDELs
+# c) extracting INDELs
 gatk SelectVariants -R human.hg38.chr16.fa -V dad.raw.vcf -select-type INDEL -O dad.indels.vcf
 
-#applying hard filters to indels
+# d) applying hard filters to indels
 gatk VariantFiltration -R human.hg38.chr16.fa -V dad.indels.vcf -O dad.filtered_indels.vcf --filter-name "indel_filter1" --filter-expression "QD < 2.0" --filter-name "indel_filter2" --filter-expression "ReadPosRankSum < -20.0" --filter-name "indel_filter3" --filter-expression "FS > 200.0" --filter-name "indel_filter4" --filter-expression "SOR > 10.0"
 
-##merge two files
+# 4.4. merge two files
 
 picard MergeVcfs I=dad.filtered_SNPs.vcf I= dad.filtered_indels.vcf O=dad.filtered_SNP_indels.vcf
 
-#keep only selected variants
+# 4.5. keep only selected variants
 gatk SelectVariants -R human.hg38.chr16.fa -V dad.filtered_SNP_indels.vcf -O dad.final.vcf --exclude-filtered
 
 ## Free to use/modify/resuse for personal academic purposes
-##© 2019 American Academy for Biomedical Informatics, Confidential and Proprietary 
+##© 2020 American Academy for Biomedical Informatics, Confidential and Proprietary 
 
